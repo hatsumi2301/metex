8b44784e0d38085a700bf3085f77c66231d5ec8e
Hatsumi Hashinaga <hatsumih@hotmail.com>
2019-10-01 22:37:05 -0300

    get temperatures from cities

--- .elixir_ls/build/test/lib/metex/ebin/metex.app
+++ .elixir_ls/build/test/lib/metex/ebin/metex.app
@@ -1,6 +1,6 @@
 {application,metex,
              [{applications,[kernel,stdlib,elixir,logger]},
               {description,"metex"},
-              {modules,['Elixir.Metex']},
+              {modules,['Elixir.Metex','Elixir.Metex.Worker']},
               {registered,[]},
               {vsn,"0.1.0"}]}.
---
+++ lib/worker.ex
@@ -0,0 +1,49 @@
+defmodule Metex.Worker do
+  def loop do
+    receive do
+      {sender_pid, location} ->
+        send(sender_pid, {:ok, temperature_of(location)})
+
+      _ ->
+        IO.puts("don't know how to process this message")
+    end
+
+    loop()
+  end
+
+  @spec temperature_of(String.t()) :: String.t()
+  def temperature_of(location) do
+    result = location |> url_for |> HTTPoison.get() |> parse_response
+
+    case result do
+      {:ok, temp} ->
+        "#{location}: #{temp}°C"
+
+      :error ->
+        "#{location} not found"
+    end
+  end
+
+  defp url_for(location) do
+    location = URI.encode(location)
+    apikey = System.get_env("WEATHER_API_KEY")
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey}"
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode!() |> compute_temperature
+  end
+
+  defp parse_response(_) do
+    :error
+  end
+
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+end
